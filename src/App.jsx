95abb8cc7be4d9d7279b935549928f6f85129022
linehub59 <linehub59@gmail.com>
2026-05-16 09:21:29 +0300

    an2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
 function App() {
   return (
-    <div>
-      <h1>Hello React + Vite</h1>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Home</Link> | 
+        <Link to="/about">About</Link> | 
+        <Link to="/contact">Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
